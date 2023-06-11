8962bf448299be42905ae8cee1140dcca5ccf5cc
gabbyb21 <gabbailo94@gmail.com>
2023-06-11 12:36:48 -0400

    completed interactions

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 $(() => {
+
   $('.dropdown').click(function() {
     $(this).next('ul.child').slideToggle('slow');
+  });
+
+  $('#project1').mouseover(function() {
+    $(this).css({'background-image': 'url(images/project1.png)', 'background-position': 'bottom'});
+  }).mouseout(function() {
+    $(this).css({'background-image': 'none'});
+  })
+
+  $('#project2').mouseover(function() {
+    $(this).css({'background-image': 'url(images/project2.png)'});
+  }).mouseout(function() {
+    $(this).css({'background-image': 'none'});
+  })
+
+  $('#project3').mouseover(function() {
+    $(this).css({'background-image': 'url(images/project3.png)'});
+  }).mouseout(function() {
+    $(this).css({'background-image': 'none'});
+  })
+
+  $('.body').click(function () {
+    $(this).css({'background-color': 'black', 'color': 'white'});
   })
 
 });
